50ed43d37fa7ce463ad49bdef877234eb59dea77
Gamaliel Garcia <46827955+EGAMAGZ@users.noreply.github.com>
2023-06-20 06:53:09 -0600

    Development (#2)
    * Movimiento de personaje y camara fija
    
    * Agregado assets de cuadros
    
    * Ejemplo de pintura personalizado
    
    * Agregada Interaccion
    
    * Intento de cortina
    
    * Creada interacción para ver significado de pintura
    
    * Pequeño cambio

---
+++ Assets/Scripts/CameraInteraction.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraInteraction : MonoBehaviour
+{
+    [Header("Player Camera")]
+    public Transform camera;
+
+    [Header("Ray Distance")]
+    public float rayDistance = 4f;
+
+    [Header("Interactable Layer")]
+    public LayerMask interactableLayer;
+
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(camera.position, camera.forward * rayDistance, Color.red);
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(camera.position, camera.forward, out hit, rayDistance, interactableLayer))
+            {
+                Debug.Log(hit.transform.name);
+                hit.transform.GetComponent<Interactable>().Interact();
+            }
+        }
+    }
+}
